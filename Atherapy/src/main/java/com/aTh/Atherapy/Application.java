043bb4d7cb9831b167e2e10aaaa828e572281e6f
gabedailey10 <gabedailey10@gmail.com>
2024-08-06 22:46:23 -0400

    added JWT authintication, gonna automate tests next

--- Atherapy/src/main/java/com/aTh/Atherapy/Application.java
+++ Atherapy/src/main/java/com/aTh/Atherapy/Application.java
@@ -1,13 +1,15 @@
 package com.aTh.Atherapy;
 
+import com.aTh.Atherapy.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
